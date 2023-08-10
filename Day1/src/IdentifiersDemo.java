70910cf869f5427349e17f96690c0f1f770a1653
USER <USER@DESKTOP-QLQ3LAB>
2023-08-10 19:31:57 +0530

    First commit

---
+++ Day1/src/IdentifiersDemo.java
@@ -0,0 +1,11 @@
+
+public class IdentifiersDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String studentname = "Aniket";	// use camelcase
+		System.out.println(studentname);
+
+	}
+
+}
